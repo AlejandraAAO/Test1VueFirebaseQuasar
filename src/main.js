5b6f2cb26a3ca9a7fd3b55c49feaa2ad5ff32ce3
AlejandraAAO <aamezaga@disnovo.com>
2019-12-08 12:44:28 -0500

    init config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+//requerir vuefire y utilizarlo
+import { rtdbPlugin } from 'vuefire';
+
+Vue.use(rtdbPlugin);
+
+import './quasar'
+
+
 
 new Vue({
   render: h => h(App),
